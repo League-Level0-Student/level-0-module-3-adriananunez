7eecd89a5f5706b531eb6c6b898571b6da1e1996
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-03 10:55:48 -0800

    RobotColorChoser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -5,23 +5,40 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-
+		 Robot r2d2=new Robot();
 		//3. ask the user what color they would like the robot to draw
-				String color =JOptionPane.show
+				String color =JOptionPane.showInputDialog("What color do you want");
+		
 		//4. use an if/else statement to set the pen color that the user requested
-
+				if(color.equals("cyan")) {
+				r2d2.setPenColor(Color.CYAN);
+				}
+				else if (color.equals("magenta")){
+				r2d2.setPenColor(Color.MAGENTA);
+				}
+				else{
+					r2d2.setRandomPenColor();
+				
+				}
+				
+				
         //5. if the user doesn’t enter anything, choose a random color
 
         //6. put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//2. set the pen width to 10
-		
+		r2d2.setPenWidth(10);
 	    //1. make the robot draw a shape (this will take more than one line of code)
-			 Robot r2d2=new Robot();
+		r2d2.penDown();
+		
+		
 		r2d2.move(70);
 			r2d2.move(120);
 			r2d2.move(70);
