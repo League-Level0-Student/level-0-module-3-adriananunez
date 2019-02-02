21e389fed7014ffe49f8f664c3131fe0d06e830e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-02 10:49:19 -0800

    Banana Quiz

--- src/extras/BananaQuiz.java
+++ src/extras/BananaQuiz.java
@@ -10,10 +10,17 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String input=JOptionPane.showInputDialog("Do you like bananas??");
 		//2. if they say no, 
+		if (input.equals("no")) {JOptionPane.showInputDialog(null, "You are Crazy!!!");
+		}
 			//tell them they are crazy 
 			//and end quiz
 		//3. if they say yes
+		if (input.equals("yes")) {
+			String hobby=JOptionPane.showInputDialog(null, "What is your favorite hobby!!!");
+			JOptionPane.showMessageDialog(null,hobby+" youruch better with bananas!");
+		}
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
 
