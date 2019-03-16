21819ac371bbaae92e5aecb2ca8b42ab3bd4bc18
ilabstaff <ilabstaff@172.16.116.67>
2019-03-16 10:55:31 -0700

    Banananannanannannannannananans

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -18,23 +18,30 @@ public class BirthdayReminder {
 		// 2. Find out which birthday the user wants and and store their response in a variable
 				String input=JOptionPane.showInputDialog("Which birthday do you want?");
 		// 3. Print out what the user typed
-				int Birthday=Integer.parseInt(input);
 		// 4. if user asked for "mom"
 			//print mom's birthday
 				
-				if (Birthday==momsBirthday) {JOptionPane.showInputDialog("Your moms birthday is on January 24th!!");
+				if (input.equals("mom")) {JOptionPane.showInputDialog("Your moms birthday is on January 24th!!");
 				
 				}
 		// 5. if user asked for "dad"
 			// print dad's birthday
-				else if (Birthday==dadsBirthday) {JoptionPane.showInputDialog("Your dads birthday is on June 9th");
+				else if (input.equals("dad")) {JOptionPane.showInputDialog("Your dads birthday is on June 9th!!!!!");
 				}
 				
 		// 6. if user asked for your name
-			
+				else if (input.equals("my birthday")) {JOptionPane.showInputDialog("Your birthday is on September 1st!!!!!!");
+				}
 
 				
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+	
+				else if (input.equals("any other birthday")) {JOptionPane.showInputDialog("Sorry I do not remember that birthday!!!!!");
+				}
+	
+				
+				
+				
+				
 	} 
 }
