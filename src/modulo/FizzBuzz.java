5994835f82443a6e8ba2f285ab60a4e57ff47d0a
ilabstaff <ilabstaff@172.16.116.68>
2019-04-06 10:55:08 -0700

    FIZZBUZZ

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -31,19 +31,28 @@ public static void main(String[] args) {
 		
 	for(int i=1;i<21;i++) {
 	if(i%3==0){	
+	
 	System.out.println("fizz");
 	}else {System.out.println(i);
 		
 	
 	
+	int=20;
+	if(i%5==0) {
+		System.out.println("buzz");
+	}
+	
+	
 	 
 		
 	
-	if (i%5==0){	
-		System.out.println("buzz");
+	
+	
+	
+	
+
 		
-		
-		}}}}}
+		}}}}
 	
 		
 	
